fda9f7116df0261ad89815d6b3df26da35d828e5
Punit Raj <raj.punit3299@gmail.com>
2020-11-18 17:46:49 +0530

    footer and responsiveness updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { GenreComponent } from './components/genre/genre.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    GenreComponent
+    GenreComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
